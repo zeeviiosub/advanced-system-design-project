5ca0ce91bb6f4f7cfe5d7931056912cd2f02ec1b
Zeevi Iosub <zeevi.iosub@gmail.com>
2020-04-29 15:41:54 +0300

    commit

--- web/app.py
+++ web/app.py
@@ -16,9 +16,21 @@ def user_page(user_id):
 
 @app.route('/<user_id>/<snapshot_id>')
 def snapshot_page(user_id, snapshot_id):
-    snap = snaplist.snapshot(user_id, snapshot_id)
+    t_x = snaplist.translation_x(user_id, snapshot_id)
+    t_y = snaplist.translation_y(user_id, snapshot_id)
+    t_z = snaplist.translation_z(user_id, snapshot_id)
+    r_x = snaplist.rotation_x(user_id, snapshot_id)
+    r_y = snaplist.rotation_y(user_id, snapshot_id)
+    r_z = snaplist.rotation_z(user_id, snapshot_id)
+    r_w = snaplist.rotation_w(user_id, snapshot_id)
+    col = snaplist.color_image(user_id, snapshot_id)
+    dep = snaplist.depth_image(user_id, snapshot_id)
+    hunger, thirst, exhaustion, happiness = \
+        snaplist.feelings(user_id, snapshot_id)
     return render_template('snap.html', \
-        title=f"{user_id}'s snapshot {snapshot_id}", user_id=user_id, \
-        snapshot_id=snapshot_id)
+        title=f"{user_id}'s snapshot {snapshot_id}", translation_x=t_x, \
+        translation_y=t_y, translation_z=t_z, rotation_x=r_x, \
+        rotation_y=r_y, rotation_z=r_z, rotation_w=r_w, hunger=hunger, \
+        thirst=thirst, exhaustion=exhaustion, happiness=happiness)
 
 app.run(host='127.0.0.1', port=8000)
--- web/snaplist.py
+++ web/snaplist.py
@@ -14,12 +14,52 @@ def snapshot(user_id, snapshot_id):
 
 def pose(user_id, snapshot_id):
     r = requests.get(f'{URL}/users/{user_id}/snapshots/{snapshot_id}/pose')
-    translation = r[0:3]
-    rotation = r[3:7]
     return r.json()['result']
+
+def translation_x(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[0]
+
+def translation_y(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[1]
+
+def translation_z(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[2]
+
+def rotation_x(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[3]
+
+def rotation_y(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[4]
+
+def rotation_z(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[5]
+
+def rotation_w(user_id, snapshot_id):
+    return pose(user_id, snapshot_id)[6]
 
 def depth_image(user_id, snapshot_id):
     r = requests.get(
-        f'{URL}/users/{user_id}/snapshots/{snapshot_id}/depth_image')
-    image = requests.get(r.json()['result'])
-    return image.data
+        f'{URL}/users/{user_id}/snapshots/{snapshot_id}/depth-image')
+    return r.json()['result']
+
+def color_image(user_id, snapshot_id):
+    r = requests.get(
+        f'{URL}/users/{user_id}/snapshots/{snapshot_id}/color-image')
+    return r.json()['result']
+
+def feelings(user_id, snapshot_id):
+    r = requests.get(
+        f'{URL}/users/{user_id}/snapshots/{snapshot_id}/feelings')
+    return r.json()['result']
+
+def hunger(user_id, snapshot_id):
+    return feelings(user_id, snapshot_id)[0]
+
+def thirst(user_id, snapshot_id):
+    return feelings(user_id, snapshot_id)[1]
+
+def exhaustion(user_id, snapshot_id):
+    return feelings(user_id, snapshot_id)[2]
+
+def happiness(user_id, snapshot_id):
+    return feelings(user_id, snapshot_id)[3]
