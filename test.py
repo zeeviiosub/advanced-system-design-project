fbc86803b305da6b5e2381a93fc9f788ae055985
zeeviiosub <57300859+zeeviiosub@users.noreply.github.com>
2020-06-02 01:18:40 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import utils
-import reader
-r=reader.Reader('sample.mind.gz')
-s=iter(r)
-s0=next(s)
-c=utils.Context()
-from parsers.feelings import FeelingsParser
-p=FeelingsParser()
-p.parse(c,s0)
