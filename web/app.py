8d8fe2450ce343b6f668228cea68bea87eb039d7
Zeevi Iosub <zeevi.iosub@gmail.com>
2020-06-01 11:46:51 +0300

    monday 11:46

--- web/app.py
+++ web/app.py
@@ -1,36 +1,36 @@
 from flask import Flask
 from flask import render_template
-import snaplist
+import utils
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return app.send_static_file('main.html')
+    users = utils.users_list()
+    return render_template(
+        'main.html',
+        users=users
+        )
 
 @app.route('/<user_id>')
 def user_page(user_id):
-    snaps = snaplist.snapshots_list(user_id)
+    snaps = utils.snapshots_list(user_id)
     return render_template('snaps.html', title=f"{user_id}'s snapshots", \
         user_id=user_id, snapshots=snaps)
 
 @app.route('/<user_id>/<snapshot_id>')
 def snapshot_page(user_id, snapshot_id):
-    t_x = snaplist.translation_x(user_id, snapshot_id)
-    t_y = snaplist.translation_y(user_id, snapshot_id)
-    t_z = snaplist.translation_z(user_id, snapshot_id)
-    r_x = snaplist.rotation_x(user_id, snapshot_id)
-    r_y = snaplist.rotation_y(user_id, snapshot_id)
-    r_z = snaplist.rotation_z(user_id, snapshot_id)
-    r_w = snaplist.rotation_w(user_id, snapshot_id)
-    col = snaplist.color_image(user_id, snapshot_id)
-    dep = snaplist.depth_image(user_id, snapshot_id)
-    hunger, thirst, exhaustion, happiness = \
-        snaplist.feelings(user_id, snapshot_id)
-    return render_template('snap.html', \
-        title=f"{user_id}'s snapshot {snapshot_id}", translation_x=t_x, \
-        translation_y=t_y, translation_z=t_z, rotation_x=r_x, \
-        rotation_y=r_y, rotation_z=r_z, rotation_w=r_w, hunger=hunger, \
-        thirst=thirst, exhaustion=exhaustion, happiness=happiness)
+    pose = utils.pose(user_id, snapshot_id)
+    feelings = utils.feelings(user_id, snapshot_id)
+    color_image = utils.color_image(user_id, snapshot_id)
+    depth_image = utils.depth_image(user_id, snapshot_id)
+    return render_template(
+        'snap.html',
+        title=f"{user_id}'s snapshot {snapshot_id}",
+        pose=pose,
+        feelings=feelings,
+        color_image=color_image,
+        depth_image=depth_image
+        )
 
-app.run(host='127.0.0.1', port=8000)
+app.run(host='127.0.0.1', port=7000)
---
+++ web/utils.py
@@ -0,0 +1,40 @@
+import requests
+
+host_ip = '127.0.0.1'
+port = '9000'
+URL = f'http://{host_ip}:{port}'
+
+def users_list():
+    r = requests.get(f'{URL}/users')
+    print(r)
+    print(r.json())
+    return r.json()['user']
+
+def snapshots_list(user_id):
+    r = requests.get(f'{URL}/users/{user_id}/snapshots')
+    return r.json()['snapshots']
+
+def snapshot(user_id, snapshot_id):
+    r = requests.get(f'{URL}/users/{user_id}/snapshots/{snapshot_id}')
+    return r.json()['snapshot']
+
+def result(user_id, snapshot_id, result):
+    r = requests.get(f'{URL}/users/{user_id}/snapshots/{snapshot_id}/{result}')
+    try:
+        return r.json()['result']
+    except Exception:
+        return ''
+
+def pose(user_id, snapshot_id):
+    return result(user_id, snapshot_id, 'pose')
+
+
+def depth_image(user_id, snapshot_id):
+    return result(user_id, snapshot_id, 'depth_image')
+
+def color_image(user_id, snapshot_id):
+    return result(user_id, snapshot_id, 'color_image')
+
+def feelings(user_id, snapshot_id):
+    return result(user_id, snapshot_id, 'feelings')
+
