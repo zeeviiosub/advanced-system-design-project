84ba1864569316abfe1f9f8fba3e46e5dd8b1735
zeeviiosub <57300859+zeeviiosub@users.noreply.github.com>
2020-06-07 00:49:40 +0300

    Update run-pipeline.sh

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -4,11 +4,11 @@ docker build -t api -f Dockerfile.api .
 docker build -t gui -f Dockerfile.gui .
 docker build -t server -f Dockerfile.server .
 docker build -t saver -f Dockerfile.saver .
-docker build -t user-parser -f Dockerfile.parsers.user
-docker build -t pose-parser -f Dockerfile.parsers.pose
-docker build -t feelings-parser -f Dockerfile.parsers.feelings
-docker build -t color-image-parser -f Dockerfile.parsers.color_image
-docker build -t depth-image-parser -f Dockerfile.parsers.depth_image
+docker build -t user-parser -f Dockerfile.parsers.user .
+docker build -t pose-parser -f Dockerfile.parsers.pose .
+docker build -t feelings-parser -f Dockerfile.parsers.feelings .
+docker build -t color-image-parser -f Dockerfile.parsers.color_image .
+docker build -t depth-image-parser -f Dockerfile.parsers.depth_image .
 docker run -d --net host api
 docker run -d -v `pwd`/web/static:/web/static --net host gui
 docker run -d -v `pwd`/web/static:/web/static --net host server
