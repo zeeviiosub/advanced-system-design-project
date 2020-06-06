a2f7102df45a66d1d723edceb957a6682e4e7e16
zeeviiosub <57300859+zeeviiosub@users.noreply.github.com>
2020-06-07 00:33:11 +0300

    Update run-pipeline.sh

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,20 +1,20 @@
-docker run -d -p 5672:5672 rabbitmq
-docker run -d -p 6379:6379 redis
-sleep 10
-python -m api run-server &
-sleep 1
-python -m gui run-server &
-sleep 1
-python -m server run-server 127.0.0.1 &
-sleep 1
-python -m saver run-saver redis://127.0.0.1:6379 rabbitmq://127.0.0.1:5672 &
-sleep 1
-python -m parsers run-parser user 127.0.0.1 &
-sleep 1
-python -m parsers run-parser pose 127.0.0.1 &
-sleep 1
-python -m parsers run-parser feelings 127.0.0.1 &
-sleep 1
-python -m parsers run-parser color_image 127.0.0.1 &
-sleep 1
-python -m parsers run-parser depth_image 127.0.0.1 &
+docker run -d --net host rabbitmq
+docker run -d --net host redis
+docker build -t api -f Dockerfile.api .
+docker build -t gui -f Dockerfile.gui .
+docker build -t server -f Dockerfile.server .
+docker build -t saver -f Dockerfile.saver .
+docker build -t user-parser -f Dockerfile.parsers.user
+docker build -t pose-parser -f Dockerfile.parsers.pose
+docker build -t feelings-parser -f Dockerfile.parsers.feelings
+docker build -t color-image-parser -f Dockerfile.parsers.color_image
+docker build -t depth-image-parser -f Dockerfile.parsers.depth_image
+docker run -d --net host api
+docker run -d -v `pwd`/web/static:/web/static --net host gui
+docker run -d -v `pwd`/web/static:/web/static --net host server
+docker run -d --net host saver
+docker run -d --net host user-parser
+docker run -d --net host pose-parser
+docker run -d --net host feelings-parser
+docker run -d --net host color-image-parser
+docker run -d --net host depth-image-parser
